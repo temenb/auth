ca45aa25735ee9b9ffef596e99156e96063d1d6a
Oleksii Temchenko <OTemchenko@fxcess.com>
2025-09-27 23:45:24 +0300

    stable

--- src/grpc/handlers/auth.handler.ts
+++ src/grpc/handlers/auth.handler.ts
@@ -1,9 +1,6 @@
 import * as grpc from '@grpc/grpc-js';
 import * as AuthGrpc from '../../generated/auth';
-import * as HealthGrpc from '../../generated/common/health';
-import * as EmptyGrpc from '../../generated/common/empty';
 import * as authService from '../../services/auth.service';
-import * as heathService from '../../services/health.service';
 
 export const callbackError = (callback: grpc.sendUnaryData<any>, err: unknown) => {
   const message = err instanceof Error ? err.message : 'Unknown error';
@@ -85,62 +82,6 @@ export const logout = async (
 
   try {
     const response = await authService.logout(userId);
-
-    callback(null, response);
-
-  } catch (err: any) {
-    callbackError(callback, err);
-  }
-};
-
-export const health = async (
-  call: grpc.ServerUnaryCall<EmptyGrpc.Empty, HealthGrpc.HealthReport>,
-  callback: grpc.sendUnaryData<HealthGrpc.HealthReport>
-) => {
-  try {
-    const response = await heathService.health();
-
-    callback(null, response);
-
-  } catch (err: any) {
-    callbackError(callback, err);
-  }
-};
-
-export const status = async (
-  call: grpc.ServerUnaryCall<EmptyGrpc.Empty, HealthGrpc.StatusInfo>,
-  callback: grpc.sendUnaryData<HealthGrpc.StatusInfo>
-) => {
-  try {
-    const response = await heathService.status();
-
-    callback(null, response);
-
-  } catch (err: any) {
-    callbackError(callback, err);
-  }
-};
-
-export const livez = async (
-  call: grpc.ServerUnaryCall<EmptyGrpc.Empty, HealthGrpc.LiveStatus>,
-  callback: grpc.sendUnaryData<HealthGrpc.LiveStatus>
-) => {
-  try {
-    const response = await heathService.livez();
-
-    callback(null, response);
-
-  } catch (err: any) {
-    callbackError(callback, err);
-  }
-};
-
-export const readyz = async (
-  call: grpc.ServerUnaryCall<EmptyGrpc.Empty, HealthGrpc.ReadyStatus>,
-  callback: grpc.sendUnaryData<HealthGrpc.ReadyStatus>
-) => {
-  try {
-    const response = await heathService.readyz();
 
     callback(null, response);
 

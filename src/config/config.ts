710047f3153f6a4f72627b4d9534fe5b3e772188
Oleksii Temchenko <OTemchenko@fxcess.com>
2026-04-06 05:21:00 +0300

    stable

--- src/config/config.ts
+++ src/config/config.ts
@@ -7,7 +7,7 @@ export const config = {
   refreshTokenSecret: process.env.JWT_REFRESH_SECRET!,
   accessTokenExpiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN || '15m',
   refreshTokenExpiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN || '7d',
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 50051,
 };
 
 export default config;

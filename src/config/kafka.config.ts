bdd6c2e0606671d91b5b93777628518c200b5fa6
Oleksii Temchenko <OTemchenko@fxcess.com>
2026-05-09 14:27:31 +0300

    unstable. kafka consumer doesn't work

--- src/config/kafka.config.ts
+++ src/config/kafka.config.ts
@@ -1,7 +1,7 @@
 const kafkaConfig = {
   brokers: (process.env.KAFKA_BROKERS?.split(',') ?? ['kafka:9092']),
-  clientId: process.env.KAFKA_CLIENT_ID || 'user-client',
-  groupId: process.env.KAFKA_GROUP_ID || 'user-service',
+  clientId: process.env.KAFKA_CLIENT_ID || 'auth-client',
+  groupId: process.env.KAFKA_GROUP_ID || 'auth-service',
 };
 
 
